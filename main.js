390aae9d3801e53f051e1aa807bb753764b4e90a
Owner <monetcarriere@yahoo.com>
2019-07-02 08:57:05 -0700

    final

---
+++ main.js
@@ -0,0 +1,81 @@
+
+alert("Hello. My name is Tod.");
+
+var name = prompt ("What is your name?")
+
+alert ("Hey" + " " + name + "!")
+
+
+
+var cont = confirm ("Want to know a secret?")
+
+if (cont) {
+	alert ("I'm a ghost.")
+
+	alert("I've been haunting this place for about six cohorts now.")
+
+
+
+	var cont = confirm ("Want to know how I died?")
+	if (cont) {
+	alert("I took one look at my homework assignment grade and had a heart attack.")
+
+	alert("That's not what actually killed me though...")
+
+	alert("One night, I was upstairs studying advanced asynchronous concepts and I became so stressed I had a brain aneurysm.")
+
+	alert("Yep. It was Javascript that took me out.")
+	} else {
+	alert ("No hard feelings.")
+}
+
+
+
+
+
+
+var cont = confirm ("Will you avenge my death?")
+
+if (cont) {
+	alert ("That's great to hear!")
+
+	alert ("It's actually very simple...")
+
+	alert("All I need you to do is answer this question for me. My soul WON'T cross over until I know that answer to this question!...")
+
+	var name = prompt ("How much wood could a woodchuck chuck if a woodchuck could chuck wood? ")
+
+	alert("Thanks. I can die happy now.")
+
+	alert ("FIN.")
+
+	
+	} else {
+	alert("Suit yourself.")
+
+}
+
+
+
+
+
+
+
+
+} else {
+	alert ("Awe man. ...okay.")
+
+}
+
+
+
+
+//alert ("It's actually very simple...")
+
+
+
+
+
+
+
+
